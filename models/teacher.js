9a775490a0b8b97a6b284476e9069908c6fc4e9f
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-11-19 20:55:46 +0700

    amin

--- models/teacher.js
+++ models/teacher.js
@@ -3,7 +3,25 @@ module.exports = (sequelize, DataTypes) => {
   const Teacher = sequelize.define('Teacher', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate:{
+        isEmail: true,
+        isUnique(value){
+          return Teacher.findAll({
+           where:{
+             email: value
+           }
+         })
+         .then(data => {
+           // console.log(" =====> ",data)
+           if(data.length !== 0){
+           throw new Error(`Email is registered`)
+         }})
+         .catch(err => { throw new Error (err) })
+       }
+      }
+    },
     SubjectId: DataTypes.INTEGER
   }, {});
   Teacher.associate = function(models) {
--- routes/teacher.js
+++ routes/teacher.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const Model = require("../models/index");
 
 routes.get("/", (req,res) =>{
+
     Model.Teacher.findAll({
         include:[{
             model: Model.Subject
@@ -25,8 +26,61 @@ routes.get("/", (req,res) =>{
         // res.send(input)
     })
     .catch(err =>{
+        console.log(err)
         res.send(err)
     })
+})
+
+routes.get("/add", (req,res) =>{
+    res.render("./pages/addTeacher.ejs")
+})
+
+routes.post("/add", (req,res) =>{
+    // res.send(req.body)
+    Model.Teacher.create(req.body)
+        .then(() => 
+        res.redirect("/teacher"))
+        .catch((err)=>
+        res.send(err))
+})
+
+routes.get("/edit/:id", (req,res) =>{
+    Model.Teacher.findOne({
+        where:{
+            id: req.params.id
+        }
+    })
+    .then(data =>{
+        let input={
+            data:data
+        }
+        // res.send(input)
+        res.render("./pages/editTeacher.ejs",input)
+    })
+    .catch(err =>{
+        res.send(err)
+    })
+})
+
+routes.post("/edit/:id", (req,res) =>{
+    Model.Teacher.update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        SubjectId: req.body.SubjectId
+    },{
+        where:
+        {
+            id:req.params.id
+        }
+    })
+    .then(() =>{
+        res.redirect("/")
+    })
+    .catch((err) =>{
+        res.send(err)
+    })
+    // res.send(req.body)
 })
 
 module.exports = routes
